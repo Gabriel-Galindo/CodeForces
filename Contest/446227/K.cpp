4dd8f8aca5e604a3275428c7db1b024483f568cf
Gabriel-Galindo <95386248+Gabriel-Galindo@users.noreply.github.com>
2023-06-06 17:25:24 -0300

    Update K.cpp

--- Contest/446227/K.cpp
+++ Contest/446227/K.cpp
@@ -12,14 +12,16 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>vr[i]>>vg[i]>>vb[i];
     }
+    //caminha por todas cores existentes
     for(ri=0;ri<256;ri++){
         for(gi=0;gi<256;gi++){
             for(bi=0;bi<256;bi++){
                 mi=INF;
+                //para cada cor dado pelo problema calculaa distancia ate a cor atual e salva o valor minimo
                 for(int i=0;i<n;i++){
                     mi=min(mi,abs(vr[i]-ri)+abs(vg[i]-gi)+abs(vb[i]-bi));
                 }
-                if(ma<mi){
+                if(ma<mi){//caso o valor minimo seja maior que o valor minimo guardado atualiza a melhor cor e o valor minimo guardado
                     ma=mi;
                     r=ri;
                     g=gi;
