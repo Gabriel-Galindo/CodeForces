bf0e0c38c97efee218df4a0cf6bb124f1dc22f15
Gabriel-Galindo <95386248+Gabriel-Galindo@users.noreply.github.com>
2023-06-06 17:16:43 -0300

    Update D.cpp

--- Contest/446227/D.cpp
+++ Contest/446227/D.cpp
@@ -7,12 +7,12 @@ int main(){
     cin>>n;
     for(int i=1;i<n;i++){
         cin>>aux;
-        total+=aux;
+        total+=aux;//guarda todos os resultados anteriores para descobrir em quem cairia
     }
     for(int i=0;i<n;i++){
-        int res=(n-(total%n)+i)%n;
-        if(res>20) cout<<"-1\n";
-        else cout<<res<<"\n";
+        int res=(n-(total%n)+i)%n;//calcula quanto tem que ser o valor posto para chegar em i
+        if(res>20) cout<<"-1\n";//se for impossivel imprime impossivel
+        else cout<<res<<"\n";//se for possivel imprime o numero
     }
     return 0;
 }
