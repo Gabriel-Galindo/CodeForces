4487fecf955e0696df8c19ae5342681f27b08304
Gabriel-Galindo <95386248+Gabriel-Galindo@users.noreply.github.com>
2023-06-06 17:21:57 -0300

    Update H.cpp

--- Contest/446227/H.cpp
+++ Contest/446227/H.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-int memo[1<<20];
-
+int memo[1<<20];//guarda todas a combinacoes existentes de quantidade impar(0 e par, 1 impar) de cada letra
+//atualmente da TLE
 int converteChar(char c){
     if(c>'y') return c-'b'-5;
     if(c>'u') return c-'b'-4;
@@ -21,14 +21,14 @@ int main(){
     memo[0]=1;
     long long res=0;
     for(i=0;i<strlen(s);i++){
-        int bitShift=converteChar(s[i]);
-        at^=(1<<bitShift);
+        int bitShift=converteChar(s[i]);//calcula qual a string de bits correspondente com a letra
+        at^=(1<<bitShift);//faz ou exclusivo com o atual para mudar a paridade da letra encontrada
         h=1;
         for(j=0;j<20;j++){
-            res+=memo[at^h];
+            res+=memo[at^h];//calcula se a letra j faz com que um estado encontrado previamente ocorre novamente, se sim daquele estado ao atual faz mais uma substring possivelmente palindroma
             h=h<<1;
         }
-        res+=memo[at]++;
+        res+=memo[at]++;//calcula se o estado atual ja ocorreu
     }
     printf("%lld\n",res);
     return 0;
